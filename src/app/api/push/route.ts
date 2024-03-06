0618f3a0654d9172107a5ec336ed93580dfe6e9d
Erik Kroon Celander <ek223ur@student.lnu.se>
2024-03-06 16:11:22 +0100

    add log

--- src/app/api/push/route.ts
+++ src/app/api/push/route.ts
@@ -2,6 +2,7 @@ import { NextResponse, NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   const data = await req.json();
+  console.log("data  ", data);
 
   return new NextResponse(`${data.test}`);
 }
