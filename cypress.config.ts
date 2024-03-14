34f36e9081ed624ec66ea9aa2f4caf58452bf224
Erik Kroon Celander <ek223ur@student.lnu.se>
2024-03-14 16:22:47 +0100

    add cypress config file

---
+++ cypress.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  env: {
+    localhost_url: "http://localhost:3000",
+    yggio_url: "https://staging.yggio.net",
+    username: "ek223ur",
+    password: "",
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
