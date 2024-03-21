7f1d695c686d533ff1f3f7bbfd88a2023bc6e09d
Henrik Tunedal <ht222mv@student.lnu.se>
2024-03-21 23:05:37 +0100

    Use APP_URL to toggle secure cookies

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -10,7 +10,8 @@ export const sessionOptions = {
   password: process.env.SECRET_COOKIE_PASSWORD!,
   cookieName: "iot-esg-app-session",
   cookieOptions: {
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV === "production" &&
+            ! /^http:/.test(process.env.APP_URL ?? ""),
     maxAge: 6 * 60 * 60 * 1000,
     ttl: 6 * 60 * 60 * 1000,
   },
