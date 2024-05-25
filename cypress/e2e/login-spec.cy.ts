fcf5cd30ef1fd1a77edf2bbe851a562c57643970
Henrik Tunedal <ht222mv@student.lnu.se>
2024-05-25 23:12:22 +0200

    Add Cypress test for login as property owner

--- cypress/e2e/login-spec.cy.ts
+++ cypress/e2e/login-spec.cy.ts
@@ -2,36 +2,13 @@
  * Spec requirements: A01
  */
 describe("login flow", () => {
-  it("successfully logs in and redirects back with a session", () => {
-    cy.visit(Cypress.env("localhost_url"));
-    cy.get("body")
-      .contains("nav", "log out")
-      .should("not.exist");
+  it("successfully logs in as tenant", () => {
+    cy.login(Cypress.env("tenant_username"), Cypress.env("tenant_password"))
+    cy.contains("nav a", /Temperature/)
+  })
 
-    cy.wait(1000);
-
-    cy.get('[id="username"]').type(Cypress.env("username"));
-    cy.get('input[id="password"]').type(Cypress.env("password"));
-    cy.get('button[data-testid="login-button"]').click();
-
-    // Workaround for what is probably a bug in our app.
-    // On a freshly loaded page, the first click on the login button
-    // doesn't load the start page, so we need to click a second time.
-    cy.wait(5000);
-    cy.get("body").then($body => {
-      let navs = $body.find("nav");
-      let buttons = $body.find('button[data-testid="login-button"]');
-      if (navs.length === 0 && buttons.length > 0) {
-        cy.log("Clicking the login button a second time.");
-        cy.get('button[data-testid="login-button"]').click();
-      }
-    });
-
-    cy.get("nav", {timeout: 20000})
-      .contains("log out")
-      .should("exist");
-
-    cy.url().should("not.include", "login");
-    cy.url().should("include", Cypress.env("localhost_url"));
-  });
+  it("successfully logs in as property owner", () => {
+    cy.login(Cypress.env("propertyowner_username"), Cypress.env("propertyowner_password"))
+    cy.contains("main button", /Download report/)
+  })
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -35,3 +35,36 @@
 //     }
 //   }
 // }
+
+Cypress.Commands.add("login", (username, password) => {
+  cy.visit(Cypress.env("localhost_url"));
+  cy.get("body")
+    .contains("nav", "log out")
+    .should("not.exist");
+
+  cy.wait(1000);
+
+  cy.get('[id="username"]').type(username);
+  cy.get('input[id="password"]').type(password);
+  cy.get('button[data-testid="login-button"]').click();
+
+  // Workaround for what is probably a bug in our app.
+  // On a freshly loaded page, the first click on the login button
+  // doesn't load the start page, so we need to click a second time.
+  cy.wait(5000);
+  cy.get("body").then($body => {
+    let navs = $body.find("nav");
+    let buttons = $body.find('button[data-testid="login-button"]');
+    if (navs.length === 0 && buttons.length > 0) {
+      cy.log("Clicking the login button a second time.");
+      cy.get('button[data-testid="login-button"]').click();
+    }
+  });
+
+  cy.get("nav", { timeout: 20000 })
+    .contains("log out")
+    .should("exist");
+
+  cy.url().should("not.include", "login");
+  cy.url().should("include", Cypress.env("localhost_url"));
+})
