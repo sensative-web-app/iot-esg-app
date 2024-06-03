640d5ed062073af90acfc2360855f1be20d0d080
Henrik Tunedal <ht222mv@student.lnu.se>
2024-06-03 12:32:53 +0200

    Update variables in Cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,12 @@ export default defineConfig({
   env: {
     localhost_url: "http://localhost:3000",
     yggio_url: "https://staging.yggio.net",
-    username: "ek223ur",
-    password: "test123",
+
+    // Configure these in your local cypress.env.json.
+    "tenant_username": "LNU_hyresgast_1",
+    "tenant_password": "",
+    "propertyowner_username": "lnu_fastighetsbolag_1",
+    "propertyowner_password": "",
   },
 
   video: true,
